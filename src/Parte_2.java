07abb2c2e82bfe8f64d8b14f3f966472298938e6
Gumi Bronka <hfernandezaguirre@gmail.com>
2022-07-16 17:57:23 -0500

    Cambios de prueba de practica

--- src/Parte_2.java
+++ src/Parte_2.java
@@ -1,7 +1,7 @@
 public class Parte_2 {
     public static void main(String[] args) {
         int numeroWhile = 0; //incrementa el valor hasta 3
-        while (numeroWhile <= 3) { // 0 < 3?, si, entonces suma
+        while (numeroWhile < 2) { // 0 < 2?, si, entonces suma
             System.out.println(numeroWhile);
             numeroWhile++;
         }
---
+++ src/Parte_3.java
@@ -0,0 +1,13 @@
+public class Parte_3 {
+    public static void main(String[] args) {
+        int numerodoWhile = 0; //incrementa el valor hasta 3
+
+        do {
+            System.out.println(numerodoWhile); // 2
+            numerodoWhile++; // 2 + 1 numerodoWhile = numerodoWhile + 1;
+
+        }while (numerodoWhile < 10); // 2 < 5 ? si, entonces suma hasta el 5
+
+    }
+}
+// Do while primero
